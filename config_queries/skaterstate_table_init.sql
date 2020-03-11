23ff820df61b2ffa5075c0d5f02f7c79043adc00
themikep82 <themikep@gmail.com>
2020-03-10 20:59:44 -0700

    added index on gameId

--- config_queries/skaterstate_table_init.sql
+++ config_queries/skaterstate_table_init.sql
@@ -1,18 +1,22 @@
 DROP TABLE IF EXISTS skaterStates;
 
 CREATE TABLE skaterStates(
-  gameId TEXT, 
-  shiftId TEXT,
-  period TEXT, 
-  playerId TEXT, 
-  playerName TEXT, 
-  position TEXT,
-  teamId TEXT,
-  teamName TEXT,
-  opposingPlayerId TEXT,
-  opposingplayerName TEXT,
-  opposingPosition TEXT,
-  opposingTeamId TEXT,
-  opposingTeamName TEXT,
-  iceTimeSeconds INTEGER,
-  strengthState TEXT);
+	gameId TEXT, 
+	shiftId TEXT,
+	period TEXT, 
+	playerId TEXT, 
+	playerName TEXT, 
+	position TEXT,
+	teamId TEXT,
+	teamName TEXT,
+	opposingPlayerId TEXT,
+	opposingplayerName TEXT,
+	opposingPosition TEXT,
+	opposingTeamId TEXT,
+	opposingTeamName TEXT,
+	iceTimeSeconds INTEGER,
+	strengthState TEXT);
+  
+CREATE INDEX skaterState_gameId
+	ON skaterStates
+	USING btree (gameId);
